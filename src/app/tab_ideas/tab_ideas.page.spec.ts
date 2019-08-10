8ec1762cd4b06221b07de392c7bca5499d488304
msramalho <up201403027@fe.up.pt>
2019-08-10 18:40:07 +0100

    tests

--- src/app/tab_ideas/tab_ideas.page.spec.ts
+++ src/app/tab_ideas/tab_ideas.page.spec.ts
@@ -4,21 +4,21 @@ import { IonicModule } from '@ionic/angular';
 import { TabIdeasPage } from './tab_ideas.page';
 
 describe('TabIdeasPage', () => {
-  let component: TabIdeasPage;
-  let fixture: ComponentFixture<TabIdeasPage>;
+	let component: TabIdeasPage;
+	let fixture: ComponentFixture<TabIdeasPage>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TabIdeasPage],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [TabIdeasPage],
+			imports: [IonicModule.forRoot()]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(TabIdeasPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+		fixture = TestBed.createComponent(TabIdeasPage);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	}));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
